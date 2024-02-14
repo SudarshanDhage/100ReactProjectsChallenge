d217b453e013b7de2c0ab79473be349cb050cb32
Sudarshan Dhage <86158515+SudarshanDhage@users.noreply.github.com>
2024-02-14 08:30:30 +0530

    8-quote-generator

---
+++ 8-quote-generator/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h2>Welcome to the React Quote Generator</h2>
+      <p>
+        Built and styled by{' '}
+        <a href='https://www.linkedin.com/in/sudarshan-dhage/' target='_none'>
+          Sudarshan
+        </a>
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
